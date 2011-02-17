7d6f743eee815aa50dd668ace8a494867aed088c
ilya <ilya.sukhar@gmail.com>
2011-02-16 22:55:09 -0800

    Remove <c-d>, <c-f> default mappings and related because they cause too many problems for people.

--- commands.js
+++ commands.js
@@ -100,15 +100,8 @@ function clearKeyMappingsAndSetDefaults() {
     "<c-e>": "scrollDown",
     "<c-y>": "scrollUp",
 
-    // scrollPageDown and scrollPageUp are mapped to two keys because they are very common actions so we
-    // want them to be mapped without a modifier key, but we also want to be faithful to Vim convention which
-    // has them on ctrl+D and ctrl+U.
     "d": "scrollPageDown",
     "u": "scrollPageUp",
-    "<c-d>": "scrollPageDown",
-    "<c-u>": "scrollPageUp",
-    "<c-f>": "scrollFullPageDown",
-    "<c-b>": "scrollFullPageUp",
     "r": "reload",
     "gs": "toggleViewSource",
 
