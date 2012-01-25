philc/vimium

commit fb99302b22ddcc7a2ecd7c21e43f3548d937701d

Open URLs from the background page.
Due to permissions issues, content scripts cannot use window.open() to
navigate to file:// schemes from http:// schemes.

Closes #456.

bookmarks.js
@@ -70,9 +70,9 @@ function activateBookmarkFindMode() {
         var isABookmarklet = function(url) { return url.indexOf("javascript:") === 0; }
 
         if (!self.newTab || isABookmarklet(url))
-          window.location = url;
+          chrome.extension.sendRequest({ handler: "openUrlInCurrentTab", url: url });
         else
-          window.open(url);
+          chrome.extension.sendRequest({ handler: "openUrlInNewTab", url: url });
 
         self.disable();
       },

commit c84be39117fff367596f985fa99d5bd92e60b9a0

Open bookmarklets by setting window.location.
This fixes a regression caused by fb99302.

bookmarks.js
@@ -69,7 +69,9 @@ function activateBookmarkFindMode() {
         var url = selection.url;
         var isABookmarklet = function(url) { return url.indexOf("javascript:") === 0; }
 
-        if (!self.newTab || isABookmarklet(url))
+        if (isABookmarklet(url))
+          window.location = url;
+        else if (!self.newTab)
           chrome.extension.sendRequest({ handler: "openUrlInCurrentTab", url: url });
         else
           chrome.extension.sendRequest({ handler: "openUrlInNewTab", url: url });