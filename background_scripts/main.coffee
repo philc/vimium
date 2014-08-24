philc/vimium

commit 700d35a1c4ef85f97f3176aa87807f9a586bf441

Do not call handlers which do not exist (fixed).

content_scripts/vimium_frontend.coffee
@@ -127,8 +127,9 @@ initializePreDomReady = ->
     # from the former.
     return if sender.tab and not sender.tab.url.startsWith 'chrome-extension://'
     return unless isEnabledForUrl or request.name == 'getActiveState' or request.name == 'setState'
-    sendResponse(if requestHandlers[request.name] then requestHandlers[request.name](request, sender) else undefined)
-    # Ensure the sendResponse callback is freed.
+    # registerFrame requests are delivered here, but there's no handler.
+    return if request.handler == "registerFrame"
+    requestHandlers[request.name](request, sender)
     false
 
 # Wrapper to install event listeners.  Syntactic sugar.

commit b599492ea27c8c6bab38e87ef343968f5fcf58e7

Fix passKeys bug introduced in commit 700d35a

background_scripts/main.coffee
@@ -389,6 +389,7 @@ updateActiveState = (tabId) ->
   partialIcon = "icons/browser_action_partial.png"
   chrome.tabs.get tabId, (tab) ->
     chrome.tabs.sendMessage tabId, { name: "getActiveState" }, (response) ->
+      console.log response
       if response
         isCurrentlyEnabled = response.enabled
         currentPasskeys = response.passKeys

content_scripts/vimium_frontend.coffee
@@ -125,12 +125,11 @@ initializePreDomReady = ->
   chrome.runtime.onMessage.addListener (request, sender, sendResponse) ->
     # In the options page, we will receive requests from both content and background scripts. ignore those
     # from the former.
-    console.log request.name, request
     return if sender.tab and not sender.tab.url.startsWith 'chrome-extension://'
     return unless isEnabledForUrl or request.name == 'getActiveState' or request.name == 'setState'
     # These requests are delivered to the options page, but there are no handlers there.
     return if request.handler == "registerFrame" or request.handler == "frameFocused"
-    requestHandlers[request.name](request, sender)
+    sendResponse requestHandlers[request.name](request, sender)
     false
 
 # Wrapper to install event listeners.  Syntactic sugar.