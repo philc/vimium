philc/vimium

commit 046650ba22ac12ebb18d1eb109fe694016eb77c0

Refactor CSS injection.

background_scripts/main.coffee
@@ -387,13 +387,14 @@ updateScrollPosition = (tab, scrollX, scrollY) ->
 
 chrome.tabs.onUpdated.addListener (tabId, changeInfo, tab) ->
   return unless changeInfo.status == "loading" # only do this once per URL change
-  chrome.tabs.insertCSS tabId,
+  cssConf =
     allFrames: true
     code: Settings.get("userDefinedLinkHintCss")
     runAt: "document_start"
-  , -> undefined if chrome.runtime.lastError
-  updateOpenTabs(tab)
-  updateActiveState(tabId)
+  chrome.tabs.insertCSS tabId, cssConf, ->
+    if not chrome.runtime.lastError
+      updateOpenTabs(tab)
+      updateActiveState(tabId)
 
 chrome.tabs.onAttached.addListener (tabId, attachedInfo) ->
   # We should update all the tabs in the old window and the new window.

commit 7e51e97faa4bc407ec411dd47572b1e36ad2be1e

Fix refresh open tabs and state.
Fixes refresh of state bug introduced in
046650ba22ac12ebb18d1eb109fe694016eb77c0.

background_scripts/main.coffee
@@ -393,10 +393,9 @@ chrome.tabs.onUpdated.addListener (tabId, changeInfo, tab) ->
     allFrames: true
     code: Settings.get("userDefinedLinkHintCss")
     runAt: "document_start"
-  chrome.tabs.insertCSS tabId, cssConf, ->
-    if not chrome.runtime.lastError
-      updateOpenTabs(tab)
-      updateActiveState(tabId)
+  chrome.tabs.insertCSS tabId, cssConf, -> chrome.runtime.lastError
+  updateOpenTabs(tab)
+  updateActiveState(tabId)
 
 chrome.tabs.onAttached.addListener (tabId, attachedInfo) ->
   # We should update all the tabs in the old window and the new window.