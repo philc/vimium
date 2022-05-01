1d8995d45c86efa4a13ba5e309425985535566d5
Phil Crosby <phil.crosby@gmail.com>
2022-04-30 22:07:58 -0700

    Remove some destructive stubbing
    This pollutes other unit tests based on the order in which they're run.

--- background_scripts/exclusions.js
+++ background_scripts/exclusions.js
@@ -63,6 +63,7 @@ var Exclusions = {
     Settings.set("exclusionRules", this.rules);
   },
 
+  // TODO(philc): Why does this take a `rules` argument if it's unused? Remove.
   postUpdateHook(rules) {
     // NOTE(mrmr1993): In FF, the |rules| argument will be garbage collected when the exclusions popup is
     // closed. Do NOT store it/use it asynchronously.
