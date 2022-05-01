389c2f1a6fce69dd7b4aa145e5d86ff37745d992
Phil Crosby <phil.crosby@gmail.com>
2022-04-30 20:59:26 -0700

    Deno port of settings_test.js

--- tests/unit_tests/test_helper.js
+++ tests/unit_tests/test_helper.js
@@ -1,11 +1,18 @@
-shoulda = require("../vendor/shoulda.js");
+import * as shoulda from "../vendor/shoulda.js";
+import "../../lib/utils.js";
+import "./test_chrome_stubs.js";
+
+const shouldaSubset = {
+  assert: shoulda.assert,
+  context: shoulda.context,
+  ensureCalled: shoulda.ensureCalled,
+  setup: shoulda.setup,
+  should: shoulda.should,
+  shoulda: shoulda,
+  stub: shoulda.stub,
+  returns: shoulda.returns,
+  tearDown: shoulda.tearDown,
+};
 
 // Attach shoulda's functions, like setup, context, should, to the global namespace.
-Object.assign(global, shoulda);
-
-// In a nodejs environment, stub out some essential DOM properties which are required before any of our code
-// can be loaded.
-if (typeof(window) == "undefined")
-  require("./test_chrome_stubs.js")
-
-require("../../lib/utils.js");
+Object.assign(window, shouldaSubset);
