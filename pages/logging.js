a1b827d23b3771cf522478a5a2ab8451d38fee95
Phil Crosby <phil.crosby@gmail.com>
2023-04-21 22:50:45 -0700

    Logging: Don't fetch the git branch name from disk
    I'm unsure what the utility of this is, and I can imagine it looking fishy to a Chrome Store
    reviewer.

--- pages/logging.js
+++ pages/logging.js
@@ -8,17 +8,4 @@ document.addEventListener("DOMContentLoaded", function () {
     "installDate",
     (items) => $("installDate").innerText = items.installDate.toString(),
   );
-
-  const branchRefRequest = new XMLHttpRequest();
-  branchRefRequest.addEventListener("load", function () {
-    const branchRefParts = branchRefRequest.responseText.split("refs/heads/", 2);
-    if (branchRefParts.length === 2) {
-      $("branchRef").innerText = branchRefParts[1];
-    } else {
-      $("branchRef").innerText = `HEAD detatched at ${branchRefParts[0]}`;
-    }
-    $("branchRef-wrapper").classList.add("no-hide");
-  });
-  branchRefRequest.open("GET", chrome.extension.getURL(".git/HEAD"));
-  branchRefRequest.send();
 });
