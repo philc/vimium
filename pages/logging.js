0be4752fcc8774ae1a2f2c5cc748155977137c27
Phil Crosby <phil.crosby@gmail.com>
2023-05-10 21:19:37 -0700

    Remove use of installDate
    We never made use of this.

--- pages/logging.js
+++ pages/logging.js
@@ -3,9 +3,4 @@ const $ = (id) => document.getElementById(id);
 document.addEventListener("DOMContentLoaded", function () {
   DomUtils.injectUserCss(); // Manually inject custom user styles.
   $("vimiumVersion").innerText = Utils.getCurrentVersion();
-
-  chrome.storage.local.get(
-    "installDate",
-    (items) => $("installDate").innerText = items.installDate.toString(),
-  );
 });
