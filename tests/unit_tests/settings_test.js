835fdb436221630c445254b8c58ceb73b17e63e5
Phil Crosby <phil.crosby@gmail.com>
2023-05-20 17:30:18 -0700

    Add migration for pre-v2.0.0 settings
    They used to be stored in chrome.storage.sync with the values encoded as JSON strings. This isn't
    necessary; we can store the values as plain objects.

---
+++ tests/unit_tests/settings_test.js
@@ -0,0 +1,27 @@
+import "./test_helper.js";
+import "../../lib/settings.js";
+
+context("settings", () => {
+
+  setup(async () => {
+    // Prior to Vimium 2.0.0, the settings values were encoded as JSON strings.
+    await chrome.storage.sync.set({ scrollStepSize: JSON.stringify(123) });
+  });
+
+  tearDown(async () => {
+    Settings.clear();
+  });
+
+  should("Run v2.0.0 migration when loading settings", async () => {
+    let storage = await chrome.storage.sync.get(null);
+    assert.equal("123", storage.scrollStepSize);
+    // The JSON value should've been migrated to an int when loading settings.
+    await Settings.load();
+    const settings = Settings.getSettings();
+    assert.equal(123, settings["scrollStepSize"]);
+    // When writing settings, the JSON value should be persisted back to storage.
+    await Settings.set(settings);
+    storage = await chrome.storage.sync.get(null);
+    assert.equal(123, storage.scrollStepSize);
+  });
+});
