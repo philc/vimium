fe5742ac6ff8d519bb36e18bb7764e69925063ff
Phil Crosby <phil.crosby@gmail.com>
2023-06-16 17:59:40 -0700

    Remove a defunct logging page

--- pages/logging.js
+++
@@ -1,6 +0,0 @@
-const $ = (id) => document.getElementById(id);
-
-document.addEventListener("DOMContentLoaded", function () {
-  DomUtils.injectUserCss(); // Manually inject custom user styles.
-  $("vimiumVersion").innerText = Utils.getCurrentVersion();
-});
