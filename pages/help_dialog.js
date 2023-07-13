c108f350b76305ab7121c4338c740a0de7fc0a8c
Phil Crosby <phil.crosby@gmail.com>
2023-07-13 00:04:52 -0700

    Remove Settings race condition in help dialog
    The help dialog would sometimes access the userDefinedLinkHintCss setting before settings were
    loaded.

--- pages/help_dialog.js
+++ pages/help_dialog.js
@@ -220,7 +220,8 @@ UIComponentServer.registerHandler(function (event) {
   }
 });
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", async () => {
+  await Settings.onLoaded();
   DomUtils.injectUserCss(); // Manually inject custom user styles.
 });
 
