philc/vimium

commit 02a08cf5978fac6d17f51c8cf6bc79051b35296e

Completions: populate vomnibar with recent tabs in Vomnibar.activateTabSelection command
Fixes #4326.

background_scripts/completion.js
@@ -476,12 +476,10 @@ class DomainCompleter {
 }
 
 // Searches through all open tabs, matching on title and URL.
+// If the query is empty, then return a list of open tabs, sorted by recency.
 class TabCompleter {
   async filter({ queryTerms }) {
-    if (queryTerms.length == 0) return [];
-
-    // NOTE(philc): We search all tabs, not just those in the current window. I'm not sure if this
-    // is the correct UX.
+    // We search all tabs, not just those in the current window.
     const tabs = await chrome.tabs.query({});
     const results = tabs.filter((tab) => RankingUtils.matches(queryTerms, tab.url, tab.title));
     const suggestions = results
@@ -511,7 +509,7 @@ class TabCompleter {
   }
 
   computeRelevancy(suggestion) {
-    if (suggestion.queryTerms.length) {
+    if (suggestion.queryTerms.length > 0) {
       return RankingUtils.wordRelevancy(suggestion.queryTerms, suggestion.url, suggestion.title);
     } else {
       return BgUtils.tabRecency.recencyScore(suggestion.tabId);

tests/unit_tests/completion_test.js
@@ -331,6 +331,11 @@ context("tab completer", () => {
     completer = new TabCompleter();
   });
 
+  should.only("return tabs by recency when query is empty", async () => {
+    const results = await filterCompleter(completer, []);
+    assert.equal(["tab1.com", "tab2.com"], results.map((tab) => tab.url));
+  });
+
   should("return matching tabs", async () => {
     const results = await filterCompleter(completer, ["tab2"]);
     assert.equal(["tab2.com"], results.map((tab) => tab.url));

content_scripts/vomnibar.js
@@ -62,11 +62,12 @@ const Vomnibar = {
     }
   },
 
-  // This function opens the vomnibar. It accepts options, a map with the values:
-  //   completer   - The name of the completer to fetch results from.
-  //   query       - Optional. Text to prefill the Vomnibar with.
-  //   selectFirst - Optional, boolean. Whether to select the first entry.
-  //   newTab      - Optional, boolean. Whether to open the result in a new tab.
+  // Opens the vomnibar.
+  // - options: a map with values
+  //     completer   - The name of the completer to fetch results from.
+  //     query       - Optional. Text to prefill the Vomnibar with.
+  //     selectFirst - Optional, boolean. Whether to select the first entry.
+  //     newTab      - Optional, boolean. Whether to open the result in a new tab.
   open(sourceFrameId, options) {
     this.init();
     // The Vomnibar cannot coexist with the help dialog (it causes focus issues).

commit f3eda16841dc8d527b43613010e378f131cf1411

Completions: show no results when Vomnibar.activate is first shown
Before this fix, the list of open tabs was shown when there was no query terms.
This behavior was regressed in 02a08cf5978fac6d17f51c8cf6bc79051b35296e.

background_scripts/completion.js
@@ -622,6 +622,14 @@ class MultiCompleter {
     const query = request.query;
     const queryTerms = request.queryTerms;
 
+    // The only UX where we support showing results when there are no query terms is via
+    // Vomnibar.activateTabSelection, where we show the list of open tabs by recency.
+    const isTabCompleter = this.completers.length == 1 &&
+      this.completers[0] instanceof TabCompleter;
+    if (queryTerms.length == 0 && !isTabCompleter) {
+      return [];
+    }
+
     const queryMatchesUserSearchEngine = searchEngineCompleter?.getUserSearchEngineForQuery(query);
 
     // If the user's query matches one of their custom search engines, then use only that engine to

tests/unit_tests/completion_test.js
@@ -319,6 +319,24 @@ context("domain completer (removing entries)", () => {
   });
 });
 
+context("multi completer", () => {
+  const tabs = [{ url: "tab1.com", title: "tab1", id: 1 },];
+  const tabCompleter = new TabCompleter();
+  let multiCompleter;
+
+  setup(() => {
+    stub(chrome.tabs, "query", () => tabs);
+    multiCompleter = new MultiCompleter([tabCompleter, new DomainCompleter()]);
+  });
+
+  should("return an empty list when the query is empty", async () => {
+    // Even though a TabCompleter returns results when the query is empty, a MultiCompleter which
+    // wraps a TabCompleter should not.
+    assert.equal(1, (await filterCompleter(tabCompleter, [])).length);
+    assert.equal([], (await filterCompleter(multiCompleter, [])));
+  });
+});
+
 context("tab completer", () => {
   const tabs = [
     { url: "tab1.com", title: "tab1", id: 1 },