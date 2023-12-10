65544979822353d5e61483dce818dd0c57232c82
Phil Crosby <phil.crosby@gmail.com>
2023-12-09 19:12:05 -0800

    Tests: remove outdated TODO

--- tests/dom_tests/dom_test_setup.js
+++ tests/dom_tests/dom_test_setup.js
@@ -6,10 +6,6 @@ import * as shoulda from "../vendor/shoulda.js";
 Object.assign(window, shoulda);
 window.shoulda = shoulda;
 
-// Shoulda.js doesn't support async code, so we try not to use any.
-// TODO(philc): This is outdated; we can consider using async tests now.
-Utils.nextTick = (func) => func();
-
 document.addEventListener("DOMContentLoaded", async () => {
   isEnabledForUrl = true;
   await Settings.onLoaded();
