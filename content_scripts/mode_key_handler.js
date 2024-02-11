dd7c045b1930a37293ffaa7f4a025c99c47d7f0c
Phil Crosby <phil.crosby@gmail.com>
2024-02-10 22:44:14 -0800

    Minor cleanups

--- content_scripts/mode_key_handler.js
+++ content_scripts/mode_key_handler.js
@@ -62,8 +62,8 @@ class KeyHandlerMode extends Mode {
     const isEscape = KeyboardUtils.isEscape(event);
     if (isEscape && ((this.countPrefix !== 0) || (this.keyState.length !== 1))) {
       return DomUtils.consumeKeyup(event, () => this.reset());
-      // If the help dialog loses the focus, then Escape should hide it; see point 2 in #2045.
     } else if (isEscape && HelpDialog && HelpDialog.isShowing()) {
+      // If the help dialog loses the focus, then Escape should hide it; see point 2 in #2045.
       HelpDialog.toggle();
       return this.suppressEvent;
     } else if (isEscape) {
