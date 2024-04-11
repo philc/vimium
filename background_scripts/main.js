philc/vimium

commit 231379c2196403a9087dd652cc01c47111c5cf4b

Search using browser default engine

background_scripts/main.js
@@ -542,6 +542,10 @@ const sendRequestHandlers = {
     });
   },
 
+  launch({ query, openInNewTab }) {
+    chrome.search.query({ disposition: openInNewTab ? "NEW_TAB" : "CURRENT_TAB", text: query });
+  },
+
   domReady(_, sender) {
     const isTopFrame = sender.frameId == 0;
     if (!isTopFrame) return;

pages/vomnibar.js
@@ -235,7 +235,11 @@ class VomnibarUI {
         if (isPrimarySearchSuggestion(firstCompletion)) {
           query = UrlUtils.createSearchUrl(query, firstCompletion?.searchUrl);
         }
-        this.hide(() => this.launchUrl(query, openInNewTab));
+        this.hide(() => chrome.runtime.sendMessage({
+          handler: "launch",
+          query,
+          openInNewTab,
+        }));
       } else if (isPrimarySearchSuggestion(completion)) {
         query = UrlUtils.createSearchUrl(query, completion.searchUrl);
         this.hide(() => this.launchUrl(query, openInNewTab));

commit 0fec0888f20c3104a5196c774faa1d043106f560

Fix custom search behavior when no entry is selected
This is a regression from 231379c2196403a9087dd652cc01c47111c5cf4b.

Also improve some function names and comments.

background_scripts/main.js
@@ -542,8 +542,9 @@ const sendRequestHandlers = {
     });
   },
 
-  launch({ query, openInNewTab }) {
-    chrome.search.query({ disposition: openInNewTab ? "NEW_TAB" : "CURRENT_TAB", text: query });
+  launchSearchQuery({ query, openInNewTab }) {
+    const disposition = openInNewTab ? "NEW_TAB" : "CURRENT_TAB";
+    chrome.search.query({ disposition, text: query });
   },
 
   domReady(_, sender) {

lib/url_utils.js
@@ -85,8 +85,8 @@ const UrlUtils = {
     return false;
   },
 
-  // Converts :string into a Google search if it's not already a URL. We don't bother with escaping
-  // characters as Chrome will do that for us.
+  // Converts string into a full URL if it's not already one. We don't escape characters as the
+  // browser will do that for us.
   async convertToUrl(string) {
     string = string.trim();
 
@@ -98,7 +98,9 @@ const UrlUtils = {
     } else if (await this.isUrl(string)) {
       return this.createFullUrl(string);
     } else {
-      return this.createSearchUrl(string);
+      const message = `convertToUrl: can't convert "${string}" into a URL. This shouldn't happen.`;
+      console.error(message);
+      return null;
     }
   },
 
@@ -125,10 +127,8 @@ const UrlUtils = {
     }
   },
 
-  // Create a search URL from the given :query (using either the provided search URL, or the default
-  // one).
+  // Create a search URL from the given :query using the provided search URL.
   createSearchUrl(query, searchUrl) {
-    if (!searchUrl) return;
     if (!["%s", "%S"].some((token) => searchUrl.indexOf(token) >= 0)) {
       searchUrl += "%s";
     }
@@ -139,7 +139,9 @@ const UrlUtils = {
   // Map a search query to its URL encoded form. The query may be either a string or an array of
   // strings. E.g. "BBC Sport" -> "BBC%20Sport".
   createSearchQuery(query) {
-    if (typeof query === "string") query = query.split(/\s+/);
+    if (typeof query === "string") {
+      query = query.split(/\s+/);
+    }
     return query.map(encodeURIComponent).join("%20");
   },
 };

pages/vomnibar.js
@@ -232,14 +232,19 @@ class VomnibarUI {
         // <Enter> on an empty query is a no-op.
         if (query.length == 0) return;
         const firstCompletion = this.completions[0];
+        const isPrimary = isPrimarySearchSuggestion(firstCompletion);
         if (isPrimarySearchSuggestion(firstCompletion)) {
-          query = UrlUtils.createSearchUrl(query, firstCompletion?.searchUrl);
+          query = UrlUtils.createSearchUrl(query, firstCompletion.searchUrl);
+          this.launchUrl(query);
+        } else {
+          this.hide(() =>
+            chrome.runtime.sendMessage({
+              handler: "launchSearchQuery",
+              query,
+              openInNewTab,
+            })
+          );
         }
-        this.hide(() => chrome.runtime.sendMessage({
-          handler: "launch",
-          query,
-          openInNewTab,
-        }));
       } else if (isPrimarySearchSuggestion(completion)) {
         query = UrlUtils.createSearchUrl(query, completion.searchUrl);
         this.hide(() => this.launchUrl(query, openInNewTab));

tests/unit_tests/url_utils_test.js
@@ -60,19 +60,6 @@ context("convertToUrl", async () => {
       await UrlUtils.convertToUrl("javascript:alert('25 % 20 * 25%20');"),
     );
   });
-
-  should("convert non-URL terms into search queries", async () => {
-    await Settings.load();
-    assert.equal("https://www.google.com/search?q=google", await UrlUtils.convertToUrl("google"));
-    assert.equal(
-      "https://www.google.com/search?q=go%20ogle.com",
-      await UrlUtils.convertToUrl("go ogle.com"),
-    );
-    assert.equal(
-      "https://www.google.com/search?q=%40twitter",
-      await UrlUtils.convertToUrl("@twitter"),
-    );
-  });
 });
 
 context("createSearchUrl", () => {