f73cdaab6b494fcd2c4e41a3445b292a27033b0c
Caleb Marcoux <caleb.marcoux@gmail.com>
2024-07-31 23:55:14 -0400

    Add missing 110% Firefox zoom level.

--- background_scripts/main.js
+++ background_scripts/main.js
@@ -186,7 +186,7 @@ function nextZoomLevel(currentZoom, steps) {
   // Chrome's default zoom levels.
   const chromeLevels = [0.25, 0.33, 0.5, 0.75, 0.8, 0.9, 1, 1.1, 1.25, 1.5, 1.75, 2, 2.5, 3, 4, 5];
   // Firefox's default zoom levels.
-  const firefoxLevels = [0.3, 0.5, 0.67, 0.8, 0.9, 1, 1.2, 1.33, 1.5, 1.7, 2, 2.4, 3, 4, 5];
+  const firefoxLevels = [0.3, 0.5, 0.67, 0.8, 0.9, 1, 1.1, 1.2, 1.33, 1.5, 1.7, 2, 2.4, 3, 4, 5];
 
   let zoomLevels = chromeLevels; // Chrome by default
   if (BgUtils.isFirefox()) {
