63f266d9357f23f26a60dc1d1066c9c11b09d271
Caleb Marcoux <caleb.marcoux@gmail.com>
2024-10-09 15:41:44 -0400

    Allow commands with options to be advanced (for hard reload).

--- background_scripts/commands.js
+++ background_scripts/commands.js
@@ -280,7 +280,7 @@ const Commands = {
             command,
             description: this.availableCommands[command].description,
             keys: keys,
-            advanced: this.advancedCommands.includes(command),
+            advanced: this.advancedCommands.includes(command) || this.advancedCommands.includes(`${command} ${options}`),
             options: options,
           });
         }
@@ -398,6 +398,7 @@ const Commands = {
     "zoomIn",
     "zoomOut",
     "zoomReset",
+    "reload hard"
   ],
 };
 
