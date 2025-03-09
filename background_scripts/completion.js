philc/vimium

commit 14c9d2edf5dd9b79d1185b217072139d38ba2952

Vomnibar: don't treat javascript keywords as user-defined search engines
Fixes #4396.

background_scripts/completion.js
@@ -523,13 +523,18 @@ class SearchEngineCompleter {
     CompletionSearch.cancel();
   }
 
-  // TODO(philc): Consider moving to UserSearchEngines
-  getUserSearchEngineForQuery(query) {
-    const parts = query.trimStart().split(/\s+/);
+  // Returns the UserSearchEngine for the given query. Returns null if the query does not begin with
+  // a keyword from one of the user's search engines.
+  getUserSearchEngineForQuery() {
+    const parts = this.input.value.trimStart().split(/\s+/);
     // For a keyword "w", we match "w search terms" and "w ", but not "w" on its own.
-    if (parts.length <= 1) return;
     const keyword = parts[0];
-    return UserSearchEngines.keywordToEngine[keyword];
+    if (parts.length <= 1) return null;
+    // Don't match queries for built-in properties like "constructor". See #4396.
+    if (Object.hasOwn(UserSearchEngines.keywordToEngine, keyword)) {
+      return UserSearchEngines.keywordToEngine[keyword];
+    }
+    return null;
   }
 
   refresh() {

pages/vomnibar.js
@@ -383,13 +383,19 @@ class VomnibarUI {
     return this.update();
   }
 
-  // Returns the UserSearchEngine for the given Vomnibar input. Returns null if the Vomnibar does
-  // not start with a keyword from one of the user's search engines.
+  // Returns the UserSearchEngine for the given query. Returns null if the query does not begin with
+  // a keyword from one of the user's search engines.
   getUserSearchEngineForQuery() {
     // This logic is duplicated from SearchEngineCompleter.getEngineForQueryPrefix
     const parts = this.input.value.trimStart().split(/\s+/);
+    // For a keyword "w", we match "w search terms" and "w ", but not "w" on its own.
     const keyword = parts[0];
-    return parts.length > 1 ? UserSearchEngines.keywordToEngine[keyword] : null;
+    if (parts.length <= 1) return null;
+    // Don't match queries for built-in properties like "constructor". See #4396.
+    if (Object.hasOwn(UserSearchEngines.keywordToEngine, keyword)) {
+      return UserSearchEngines.keywordToEngine[keyword];
+    }
+    return null;
   }
 
   queryIsCustomSearch() {

tests/dom_tests/vomnibar_test.js
@@ -69,4 +69,15 @@ context("vomnibar", () => {
     assert.equal("launchSearchQuery", handler);
     assert.equal("example", query);
   });
+
+  // This test covers #4396.
+  should("not treat javascript keywords as user-defined search engines", async () => {
+    const instance = new Vomnibar();
+    await instance.activate();
+    const ui = instance.vomnibarUI;
+    ui.setQuery("constructor "); // "constructor" is a built-in JS property
+    ui.onInput();
+    // The query should not be treated as a user search engine.
+    assert.equal("constructor ", ui.input.value);
+  });
 });

commit 03712d32f8b68cddd6b89527f0932f4a7088171a

Fix user-defined search engine logic error introduced in 14c9d2edf

background_scripts/completion.js
@@ -525,8 +525,8 @@ class SearchEngineCompleter {
 
   // Returns the UserSearchEngine for the given query. Returns null if the query does not begin with
   // a keyword from one of the user's search engines.
-  getUserSearchEngineForQuery() {
-    const parts = this.input.value.trimStart().split(/\s+/);
+  getUserSearchEngineForQuery(query) {
+    const parts = query.trimStart().split(/\s+/);
     // For a keyword "w", we match "w search terms" and "w ", but not "w" on its own.
     const keyword = parts[0];
     if (parts.length <= 1) return null;