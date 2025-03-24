1986b818456881024d3df8a577e9debd65dd8b55
Phil Crosby <phil.crosby@gmail.com>
2025-03-23 22:53:58 -0700

    Help dialog: use the command info data structures

--- tests/unit_tests/commands_test.js
+++ tests/unit_tests/commands_test.js
@@ -1,6 +1,7 @@
 import "./test_helper.js";
 import "../../background_scripts/tab_recency.js";
 import "../../background_scripts/bg_utils.js";
+import "../../background_scripts/all_commands.js";
 import "../../lib/settings.js";
 import "../../lib/keyboard_utils.js";
 import "../../background_scripts/commands.js";
