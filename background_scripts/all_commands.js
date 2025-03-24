b57077b0fb63a11ea45b24328a7ae40cc231e3c8
Phil Crosby <phil.crosby@gmail.com>
2025-03-23 22:50:55 -0700

    Move command metadata into all_commands.js

--- background_scripts/all_commands.js
+++ background_scripts/all_commands.js
@@ -1,10 +1,155 @@
+// This is the order they will be shown in the help dialog.
 const allCommands = [
+  //
+  // Navigation
+  //
+
   {
-    name: "showHelp",
-    desc: "Show help",
+    name: "scrollDown",
+    desc: "Scroll down",
+    group: "navigation",
+  },
+
+  {
+    name: "scrollUp",
+    desc: "Scroll up",
+    group: "navigation",
+  },
+
+  {
+    name: "scrollToTop",
+    desc: "Scroll to the top of the page",
+    group: "navigation",
+  },
+
+  {
+    name: "scrollToBottom",
+    desc: "Scroll to the bottom of the page",
+    group: "navigation",
+  },
+
+  {
+    name: "scrollPageDown",
+    desc: "Scroll a half page down",
+    group: "navigation",
+  },
+
+  {
+    name: "scrollPageUp",
+    desc: "Scroll a half page up",
+    group: "navigation",
+  },
+
+  {
+    name: "scrollFullPageDown",
+    desc: "Scroll a full page down",
+    group: "navigation",
+  },
+
+  {
+    name: "scrollFullPageUp",
+    desc: "Scroll a full page up",
+    group: "navigation",
+  },
+
+  {
+    name: "scrollLeft",
+    desc: "Scroll left",
+    group: "navigation",
+  },
+
+  {
+    name: "scrollRight",
+    desc: "Scroll right",
+    group: "navigation",
+  },
+
+  {
+    name: "scrollToLeft",
+    desc: "Scroll all the way to the left",
+    group: "navigation",
+  },
+
+  {
+    name: "scrollToRight",
+    desc: "Scroll all the way to the right",
+    group: "navigation",
+  },
+
+  {
+    name: "reload",
+    desc: "Reload the page",
+    group: "navigation",
+    background: true,
+    options: {
+      hard: "Perform a hard reload, forcing the browser to bypass its cache.",
+    },
+  },
+
+  {
+    name: "copyCurrentUrl",
+    desc: "Copy the current URL to the clipboard",
+    group: "navigation",
     noRepeat: true,
-    topFrame: true,
-    group: "misc",
+  },
+
+  {
+    name: "openCopiedUrlInCurrentTab",
+    desc: "Open the clipboard's URL in the current tab",
+    group: "navigation",
+    noRepeat: true,
+  },
+
+  {
+    name: "openCopiedUrlInNewTab",
+    desc: "Open the clipboard's URL in a new tab",
+    group: "navigation",
+    noRepeat: true,
+  },
+
+  {
+    name: "goUp",
+    desc: "Go up the URL hierarchy",
+    group: "navigation",
+  },
+
+  {
+    name: "goToRoot",
+    desc: "Go to the root of current URL hierarchy",
+    group: "navigation",
+  },
+
+  {
+    name: "enterInsertMode",
+    desc: "Enter insert mode",
+    group: "navigation",
+    noRepeat: true,
+  },
+
+  {
+    name: "enterVisualMode",
+    desc: "Enter visual mode",
+    group: "navigation",
+    noRepeat: true,
+  },
+
+  {
+    name: "enterVisualLineMode",
+    desc: "Enter visual line mode",
+    group: "navigation",
+    noRepeat: true,
+  },
+
+  {
+    name: "passNextKey",
+    desc: "Pass the next key to the page",
+    group: "navigation",
+  },
+
+  {
+    name: "focusInput",
+    desc: "Focus the first text input on the page",
+    group: "navigation",
   },
 
   {
@@ -20,27 +165,141 @@ const allCommands = [
   },
 
   {
-    name: "scrollDown",
-    desc: "Scroll down",
+    name: "LinkHints.activateModeToOpenInNewTab",
+    desc: "Open a link in a new tab",
     group: "navigation",
   },
 
   {
-    name: "copyCurrentUrl",
-    desc: "Copy the current URL to the clipboard",
+    name: "LinkHints.activateModeToOpenInNewForegroundTab",
+    desc: "Open a link in a new tab & switch to it",
+    group: "navigation",
+  },
+
+  {
+    name: "LinkHints.activateModeWithQueue",
+    desc: "Open multiple links in a new tab",
     group: "navigation",
     noRepeat: true,
   },
 
   {
-    name: "reload",
-    desc: "Reload the page",
+    name: "LinkHints.activateModeToDownloadLink",
+    desc: "Download link url",
+    group: "navigation",
+  },
+
+  {
+    name: "LinkHints.activateModeToOpenIncognito",
+    desc: "Open a link in incognito window",
+    group: "navigation",
+  },
+
+  {
+    name: "LinkHints.activateModeToCopyLinkUrl",
+    desc: "Copy a link URL to the clipboard",
+    group: "navigation",
+  },
+
+  {
+    name: "goPrevious",
+    desc: "Follow the link labeled previous or <",
+    group: "navigation",
+    noRepeat: true,
+  },
+
+  {
+    name: "goNext",
+    desc: "Follow the link labeled next or >",
+    group: "navigation",
+    noRepeat: true,
+  },
+
+  {
+    name: "nextFrame",
+    desc: "Select the next frame on the page",
     group: "navigation",
     background: true,
-    options: {
-      hard: "Perform a hard reload, forcing the browser to bypass its cache.",
-    },
   },
+
+  {
+    name: "mainFrame",
+    desc: "Select the page's main/top frame",
+    group: "navigation",
+    topFrame: true,
+    noRepeat: true,
+  },
+
+  {
+    name: "Marks.activateCreateMode",
+    desc: "Create a new mark",
+    group: "navigation",
+    noRepeat: true,
+  },
+
+  {
+    name: "Marks.activateGotoMode",
+    desc: "go to a mark",
+    group: "navigation",
+    noRepeat: true,
+  },
+
+  //
+  // Vomnibar
+  //
+
+  {
+    name: "Vomnibar.activate",
+    desc: "Open URL, bookmark or history entry",
+    group: "vomnibar",
+    topFrame: true,
+  },
+
+  {
+    name: "Vomnibar.activateInNewTab",
+    desc: "Open URL, bookmark or history entry in a new tab",
+    group: "vomnibar",
+    topFrame: true,
+  },
+
+  {
+    name: "Vomnibar.activateBookmarks",
+    desc: "Open a bookmark",
+    group: "vomnibar",
+    topFrame: true,
+  },
+
+  {
+    name: "Vomnibar.activateBookmarksInNewTab",
+    desc: "Open a bookmark in a new tab",
+    group: "vomnibar",
+    topFrame: true,
+  },
+
+  {
+    name: "Vomnibar.activateTabSelection",
+    desc: "Search through your open tabs",
+    group: "vomnibar",
+    topFrame: true,
+  },
+
+  {
+    name: "Vomnibar.activateEditUrl",
+    desc: "Edit the current URL",
+    group: "vomnibar",
+    topFrame: true,
+  },
+
+  {
+    name: "Vomnibar.activateEditUrlInNewTab",
+    desc: "Edit the current URL and open in a new tab",
+    group: "vomnibar",
+    topFrame: true,
+  },
+
+  //
+  // Find
+  //
 
   {
     name: "enterFindMode",
@@ -62,20 +321,20 @@ const allCommands = [
   },
 
   {
-    name: "createTab",
-    desc: "Create new tab",
-    group: "tabs",
-    background: true,
-    repeatLimit: 20,
+    name: "findSelected",
+    desc: "Find the selected text",
+    group: "find",
   },
 
   {
-    name: "Vomnibar.activate",
-    desc: "Open URL, bookmark or history entry",
-    group: "vomnibar",
-    topFrame: true,
+    name: "findSelectedBackwards",
+    desc: "Find the selected text, searching backwards",
+    group: "find",
   },
 
+  //
+  // History
+  //
 
   {
     name: "goBack",
@@ -84,11 +343,188 @@ const allCommands = [
   },
 
   {
-    name: "goGorward",
+    name: "goForward",
     desc: "Go forward in history",
     group: "history",
   },
 
+  //
+  // Tabs
+  //
+
+  {
+    name: "createTab",
+    desc: "Create new tab",
+    group: "tabs",
+    background: true,
+    repeatLimit: 20,
+  },
+
+  {
+    name: "previousTab",
+    desc: "Go one tab left",
+    group: "tabs",
+    background: true,
+  },
+
+  {
+    name: "nextTab",
+    desc: "Go one tab right",
+    group: "tabs",
+    background: true,
+  },
+
+  {
+    name: "visitPreviousTab",
+    desc: "Go to previously-visited tab",
+    group: "tabs",
+    background: true,
+  },
+
+  {
+    name: "firstTab",
+    desc: "Go to the first tab",
+    group: "tabs",
+    background: true,
+  },
+
+  {
+    name: "lastTab",
+    desc: "Go to the last tab",
+    group: "tabs",
+    background: true,
+  },
+
+  {
+    name: "duplicateTab",
+    desc: "Duplicate current tab",
+    group: "tabs",
+    background: true,
+    repeatLimit: 20,
+  },
+
+  {
+    name: "togglePinTab",
+    desc: "Pin or unpin current tab",
+    group: "tabs",
+    background: true,
+  },
+
+  {
+    name: "toggleMuteTab",
+    desc: "Mute or unmute current tab",
+    group: "tabs",
+    background: true,
+    noRepeat: true,
+  },
+
+  {
+    name: "removeTab",
+    desc: "Close current tab",
+    group: "tabs",
+    background: true,
+    // Don't close (in one command invocation) more than the number of tabs that can be re-opened by
+    // the browser.
+    repeatLimit: chrome.sessions?.MAX_SESSION_RESULTS || 25,
+  },
+
+  {
+    name: "restoreTab",
+    desc: "Restore closed tab",
+    group: "tabs",
+    background: true,
+    repeatLimit: 20,
+  },
+
+  {
+    name: "moveTabToNewWindow",
+    desc: "Move tab to new window",
+    group: "tabs",
+    background: true,
+  },
+
+  {
+    name: "closeTabsOnLeft",
+    desc: "Close tabs on the left",
+    group: "tabs",
+    background: true,
+  },
+
+  {
+    name: "closeTabsOnRight",
+    desc: "Close tabs on the right",
+    group: "tabs",
+    background: true,
+  },
+
+  {
+    name: "closeOtherTabs",
+    desc: "Close all other tabs",
+    group: "tabs",
+    background: true,
+    noRepat: true,
+  },
+
+  {
+    name: "moveTabLeft",
+    desc: "Move tab to the left",
+    group: "tabs",
+    background: true,
+  },
+
+  {
+    name: "moveTabRight",
+    desc: "Move tab to the right",
+    group: "tabs",
+    background: true,
+  },
+
+  {
+    name: "setZoom",
+    desc: "Set zoom",
+    group: "tabs",
+    background: true,
+  },
+
+  {
+    name: "zoomIn",
+    desc: "Zoom in",
+    group: "tabs",
+    background: true,
+  },
+
+  {
+    name: "zoomOut",
+    desc: "Zoom out",
+    group: "tabs",
+    background: true,
+  },
+
+  {
+    name: "zoomReset",
+    desc: "Reset zoom",
+    group: "tabs",
+    background: true,
+  },
+
+  //
+  // Misc
+  //
+
+  {
+    name: "toggleViewSource",
+    desc: "View page source",
+    group: "misc",
+    noRepeat: true,
+  },
+
+  {
+    name: "showHelp",
+    desc: "Show help",
+    group: "misc",
+    noRepeat: true,
+    topFrame: true,
+  },
 ];
 
 globalThis.allCommands = allCommands;
