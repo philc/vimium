825f1b14b3807686923ce21ba11ad02cc1a003c4
Phil Crosby <phil.crosby@gmail.com>
2025-03-27 22:18:19 -0700

    Command listing: show keys each command is bound to

--- pages/command_listing.js
+++ pages/command_listing.js
@@ -1,8 +1,23 @@
-function populatePage() {
+// The ordering we show key bindings is alphanumerical, except that special keys sort to the end.
+function compareKeys(a, b) {
+  a = a.replace("<", "~");
+  b = b.replace("<", "~");
+  if (a < b) {
+    return -1;
+  } else if (b < a) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+async function populatePage() {
   const h2s = document.querySelectorAll("h2");
   const byGroup = Object.groupBy(allCommands, (el) => el.group);
+  const commandToOptionsToKeys = (await chrome.storage.session.get("helpPageData")).helpPageData;
 
-  const template = document.querySelector("template#command").content;
+  const commandTemplate = document.querySelector("template#command").content;
+  const keysTemplate = document.querySelector("template#keys").content;
 
   for (const h2 of Array.from(h2s)) {
     const group = h2.dataset["group"];
@@ -10,9 +25,22 @@ function populatePage() {
     // Display them in alphabetical order.
     commands = commands.sort((a, b) => b.name.localeCompare(a.name));
     for (const command of commands) {
-      const el = template.cloneNode(true);
+      // Here, we're going to list all of the keys bound to this command, and for now, we're not
+      // going to visually distinguish versions of the command with options and versions without.
+      const keys = Object.values(commandToOptionsToKeys[command.name] || {})
+        .flat(1);
+      const el = commandTemplate.cloneNode(true);
       el.querySelector("h3 code").textContent = command.name;
+
+      const keysEl = el.querySelector(".key-bindings");
+      for (const key of keys.sort(compareKeys)) {
+        const node = keysTemplate.cloneNode(true);
+        node.querySelector(".key").textContent = key;
+        keysEl.appendChild(node);
+      }
+
       el.querySelector(".desc").textContent = command.desc;
+
       if (command.options) {
         const ul = el.querySelector(".options ul");
         for (const [name, desc] of Object.entries(command.options)) {
@@ -31,7 +59,7 @@ function populatePage() {
 document.addEventListener("DOMContentLoaded", async () => {
   await Settings.onLoaded();
   DomUtils.injectUserCss();
-  populatePage();
+  await populatePage();
   // await Commands.init();
   // await OptionsPage.init();
 });
