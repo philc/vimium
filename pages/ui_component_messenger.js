238d65c9d2d5524e2031af7f650a948394ed72c3
Phil Crosby <phil.crosby@gmail.com>
2025-04-05 15:47:39 -0700

    UIComponent: clean up code

--- pages/ui_component_messenger.js
+++ pages/ui_component_messenger.js
@@ -1,3 +1,10 @@
+//
+// These are functions for a page in a UIComponent iframe to communicate to its parent frame.
+//
+
+let ownerPagePort = null;
+let handleMessage = null;
+
 async function registerPortWithOwnerPage(event) {
   if (event.source !== globalThis.parent) return;
   // The Vimium content script that's running on the parent page has access to this vimiumsecret
@@ -8,7 +15,7 @@ async function registerPortWithOwnerPage(event) {
     Utils.debugLog("ui_component_messenger.js: vimiumSecret is incorrect.");
     return;
   }
-  portOpen(event.ports[0]);
+  openPort(event.ports[0]);
   // Once we complete a handshake with the parent page hosting this page's iframe, stop listening
   // for messages on the window object.
   globalThis.removeEventListener("message", registerPortWithOwnerPage);
@@ -18,18 +25,13 @@ export function init() {
   globalThis.addEventListener("message", registerPortWithOwnerPage);
 }
 
-let ownerPagePort = null;
-let handleMessage = null;
-let _portOpened = false;
-
-export function portOpen(port) {
+function openPort(port) {
   ownerPagePort = port;
   ownerPagePort.onmessage = async (event) => {
     if (handleMessage) {
       return await handleMessage(event);
     }
   };
-  _portOpened = true;
   dispatchReadyEventWhenReady();
 }
 
@@ -45,19 +47,19 @@ export function postMessage(message) {
 // We require both that the DOM is ready and that the port has been opened before the UIComponent
 // is ready. These events can happen in either order. We count them, and notify the content script
 // when we've seen both.
-let _dispatchedReadyEvent = false;
+let hasDispatchedReadyEvent = false;
 function dispatchReadyEventWhenReady() {
-  if (_dispatchedReadyEvent) return;
+  if (hasDispatchedReadyEvent) return;
 
   if (document.readyState === "loading") {
     globalThis.addEventListener("DOMContentLoaded", () => dispatchReadyEventWhenReady());
     return;
   }
-  if (!_portOpened) return;
+  if (!ownerPagePort) return;
 
   if (globalThis.frameId != null) {
     postMessage({ name: "setIframeFrameId", iframeFrameId: globalThis.frameId });
   }
-  _dispatchedReadyEvent = true;
+  hasDispatchedReadyEvent = true;
   postMessage("uiComponentIsReady");
 }
