philc/vimium

commit f21271b09df5d2f6573c5cb7a7a2a6d41f707a24

Update a protocol check to include Edge
I forget how/why I found this, but it was on my TODO list.

content_scripts/link_hints.js
@@ -206,7 +206,8 @@ const HintCoordinator = {
     });
     this.onExit = [onExit];
     const protocol = window.location.protocol;
-    const isExtensionPage = ["chrome-extension:", "moz-extension:"].includes(protocol);
+    // chrome-extension, moz-extension (Firefox), extension (Edge).
+    const isExtensionPage = protocol.endsWith("extension");
     chrome.runtime.sendMessage({
       handler: "prepareToActivateLinkHintsMode",
       modeIndex: availableModes.indexOf(mode),

commit 7a3c9b7b716ca3dc4515d97eb004fa33e5953e70

Fix link hints are not shown in help dialog
This is a regression from f21271b09df5d2f6573c5cb7a7a2a6d41f707a24. The protocol string ends with
colon.

content_scripts/link_hints.js
@@ -207,7 +207,7 @@ const HintCoordinator = {
     this.onExit = [onExit];
     const protocol = window.location.protocol;
     // chrome-extension, moz-extension (Firefox), extension (Edge).
-    const isExtensionPage = protocol.endsWith("extension");
+    const isExtensionPage = protocol.endsWith("extension:");
     chrome.runtime.sendMessage({
       handler: "prepareToActivateLinkHintsMode",
       modeIndex: availableModes.indexOf(mode),