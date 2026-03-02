092ce6ba42edb5ab92f6671312506a078572444c
tushar.muralidharan <tushar.muralidharan@bytedance.com>
2026-03-02 17:29:00 +1100

    feat: add tab group support (skip collapsed groups, zc/zj/zk navigation)

--- background_scripts/commands.js
+++ background_scripts/commands.js
@@ -483,6 +483,9 @@ const defaultKeyMappings = {
   "zi": "zoomIn",
   "zo": "zoomOut",
   "z0": "zoomReset",
+  "zc": "collapseTabGroup",
+  "zj": "previousTabGroup",
+  "zk": "nextTabGroup",
 
   // Marks
   "m": "Marks.activateCreateMode",
